c1037d80d34ddcd7376bd603ba97e24aa04ec20f
artyoum <michaelcoulter0474@gmail.com>
2020-06-23 20:07:22 -0400

    Update imgGrid.js
    Adds failsafes and a means to go back to a default styling

--- scripts/imgGrid.js
+++ scripts/imgGrid.js
@@ -1,17 +1,42 @@
 //Changes the style of the image grid when there are few images
 window.onload = function() {
+    //gets number of album images
     let numImages = document.getElementsByClassName("album-link").length;
+    //gets the albums' container
+    let imgContainer = document.getElementById("img-container");
 
     console.log(`Number of images: ${numImages}`);
-
-    if (numImages < 4) {
-        let imgContainer = document.getElementById("img-container");
-
-        imgContainer.style.gridTemplateColumns = "1fr 1fr";
-        imgContainer.style.width = "65%";
+    
+    let x = window.matchMedia("(max-width: 980px)");
+    
+    // Call listener function at run time
+    resizeImg(x);
+    
+    // Attach listener function on state changes
+    x.addListener(resizeImg);
+      
+    //if the user is on mobile && there are less than 4 images in the grid
+    function resizeImg(x) {
+        if (x.matches) { // If media query matches (mobile view)
+            if (numImages <= 4) {
+                imgContainer.style.gridTemplateColumns = "1fr 1fr";
+                imgContainer.style.width = "65%";
         
-
-        console.log("Changed style");
+                console.log("Slim style (mobile)");
+            } else { //resizes back to default style when 5 or more albums used
+                imgContainer.style.gridTemplateColumns = "repeat(auto-fill, minmax(250px, 1fr)";
+                imgContainer.style.width = "70%";
+            }
+        } else { //no match (desktop view)
+            if (numImages <= 4) {
+                imgContainer.style.gridTemplateColumns = "1fr 1fr";
+                imgContainer.style.width = "42%";
         
-    }
+                console.log("Slim style (widescreen)");
+            } else { //resizes back to default style when 5 or more albums used
+                imgContainer.style.gridTemplateColumns = "repeat(auto-fill, minmax(250px, 1fr)";
+                imgContainer.style.width = "70%";
+            }
+        }
+    }    
 }
