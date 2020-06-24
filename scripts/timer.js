3d0df8c80e681aa0b5c9b67f61c7506d011a802d
artyoum <michaelcoulter0474@gmail.com>
2020-06-23 20:08:32 -0400

    Update timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -23,7 +23,7 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "ALBUM DROPPED";
+    document.getElementById("timer").innerHTML = "DESPERADO - AVAILABLE NOW";
     document.getElementById("timer-title").innerHTML = "";
   }
 }, 1000);
