4ccb0e5f4ef6f0df3afbd3a57eed78a490fa76b0
artyoum <michaelcoulter0474@gmail.com>
2022-08-26 15:22:41 -0400

    Update imgGrid.js

--- scripts/imgGrid.js
+++ scripts/imgGrid.js
@@ -7,40 +7,27 @@ window.onload = function() {
 
     console.log(`Number of images: ${numImages}`);
     
-    let x = window.matchMedia("(max-width: 500px;)");
+    var x = window.matchMedia("(max-width: 700px)");
     
     // Call listener function at run time
     resizeImg(x);
     
     // Attach listener function on state changes
     x.addListener(resizeImg);
-      
+    
     //if the user is on mobile && there are less than 4 images in the grid
     function resizeImg(x) {
         if (x.matches) { // If media query matches (mobile view)
-            if (numImages <= 4) {
-                imgContainer.style.gridTemplateColumns = "1fr";
-                imgContainer.style.width = "65%";
+            imgContainer.style.gridTemplateColumns = "1fr";
+            imgContainer.style.width = "65%";
         
-                console.log("Slim style (mobile)");
-            } else { //resizes back to default style when 5 or more albums used
-                imgContainer.style.gridTemplateColumns = "repeat(auto-fill, minmax(250px, 1fr)";
-                imgContainer.style.width = "70%";
-
-                console.log("Default style: 5OOrMoreStyle");
-            }
-        } else { //no match (desktop view)
-            if (numImages <= 4) {
-                imgContainer.style.gridTemplateColumns = "1fr 1fr";
-                imgContainer.style.width = "50%";
+            console.log("Mobile View");
+        } 
+        else { //no match (desktop view)
+            imgContainer.style.gridTemplateColumns = "1fr 1fr";
+            imgContainer.style.width = "50%";
         
-                console.log("Slim style (widescreen)");
-            } else { //resizes back to default style when 5 or more albums used
-                imgContainer.style.gridTemplateColumns = "repeat(auto-fill, minmax(250px, 1fr)";
-                imgContainer.style.width = "70%";
-
-                console.log("Default style: 5OOrMoreStyle");
-            }
+            console.log("Desktop View");
         }
     }    
 }
