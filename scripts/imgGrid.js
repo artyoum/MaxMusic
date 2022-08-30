35123f042bbd07850b6ffe673743be2434f132dd
artyoum <michaelcoulter0474@gmail.com>
2022-08-30 16:25:05 -0400

    Update imgGrid.js
    Making imgGrid.js work by changing internal media query

--- scripts/imgGrid.js
+++ scripts/imgGrid.js
@@ -8,26 +8,27 @@ window.onload = function() {
     let imgContainer = document.getElementById("img-container");
     
     var x = window.matchMedia("(max-width: 700px)");
-    console.log(`Does window.matchMedia? ${x}`);
+    console.log(`Does window.matchMedia? ${x.matches}`);
     
+    //imgGrid resizing listener code starts here
     adjustMenuDesign();
     window.addEventListener("resize", adjustMenuDesign);
     
     function adjustMenuDesign() {
-        const width = window.matchMedia("(max-width: 700px)");
+        const width = window.matchMedia("all and (max-width: 980px)");
         if (width.matches) { // If media query matches (mobile view)
             imgContainer.style.gridTemplateColumns = "1fr";
-            imgContainer.style.width = "65%";
+            imgContainer.style.width = "50%";
         
+            console.log("resized");
             console.log("Mobile View");
-            alert("resized");
         } 
         else { //no match (desktop view)
             imgContainer.style.gridTemplateColumns = "1fr 1fr";
-            imgContainer.style.width = "50%";
+            imgContainer.style.width = "65%";
         
+            console.log("you fucked up again/no resize");
             console.log("Desktop View");
-            alert("you fucked up again/no resize");
         }
     }
     
