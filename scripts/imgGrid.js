a5c55b73f881b722d98c49117af177baaee4669e
artyoum <michaelcoulter0474@gmail.com>
2022-09-02 14:29:23 -0400

    Update imgGrid.js
    I Think i made this work :)
    Finally.

--- scripts/imgGrid.js
+++ scripts/imgGrid.js
@@ -43,12 +43,14 @@ window.onload = function() {
         if (x.matches) { 
             imgContainer.style.gridTemplateColumns = "1fr";
             imgContainer.style.width = "65%";
+            imgContainer.style.margin = "40px auto";
         
             console.log("Mobile View");
         } 
         else { //no match (desktop view)
             imgContainer.style.gridTemplateColumns = "1fr 1fr";
             imgContainer.style.width = "50%";
+            imgContainer.style.margin = "40px auto";
         
             console.log("Desktop View");
         }
